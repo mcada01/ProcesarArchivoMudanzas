9028da6928f475ab0d0d3b6e98812299e1e7cbaa
Melissa Cadavid Peláez <mcada01@hotmail.com>
2019-05-16 12:17:41 -0500

    Add project files.

---
+++ ProcesarArchivoMudanzas/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProcesarArchivoMudanzas.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ProcesarArchivoMudanzas/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace ProcesarArchivoMudanzas.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
